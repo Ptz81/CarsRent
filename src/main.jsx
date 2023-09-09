535aa720c06826ec844221e89ac670d9e0866bb1
Taras Papka <taras.papka@protonmail.com>
2023-09-10 00:14:38 +0300

    added styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import 'modern-normalize/modern-normalize.css';
+// import { StrictMode } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Global } from '@emotion/react';
+import { GlobalStyles } from './styles/GlobalStyles.js';
+import { store, persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  // <StrictMode>
+  <>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/project-Dream4IT">
+          <Global styles={GlobalStyles} />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    <ToastContainer
+      autoClose={2000}
+      position="top-center"
+      progressStyle={{ height: '1px' }}
+      hideProgressBar={true}
+      theme="dark"
+      toastStyle={{
+        backgroundColor: `rgb(22, 31, 55)`,
+      }}
+    />
+  </>
+  // </StrictMode>
+);
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,104 @@
+import 'react-toastify/dist/ReactToastify.css';
+import './fonts.css';
+import { css } from '@emotion/react';
+
+export const colors = {
+  grayColor: '#8A8A89',
+  subTitleColor: 'rgba(18, 20, 23, 0.5)',
+  textMainColor: '#121417',
+  accentColor: '#3470FF',
+  hoverColor: '#0B44CD',
+  mainBgColor: '#ffffff',
+  disabled: '#F7F7FB'
+};
+
+export const breakpoint = {
+  mobile: '375px',
+  tablet: '768px',
+  desktop: '1200px',
+};
+
+export const GlobalStyles = css`
+  :root {
+    --main-font-family: 'Manrope', sans-serif;
+    --transition-duration: 250ms;
+    --transition-func: ease;
+  }
+  html {
+    width: 100%;
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
+  body {
+    width: 100%;
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    color: ${colors.textMainColor};
+    font-family: var(--main-font-family);
+    background-color: ${colors.mainBgColor};
+  }
+
+  div,
+  section,
+  span,
+  object,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  fieldset,
+  form {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  nav,
+  a {
+    display: block;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    ${'' /* vertical-align: baseline; */}
+    text-decoration: none;
+  }
+
+  ol,
+  ul,
+  li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+export default {
+  colors,
+  GlobalStyles,
+};
